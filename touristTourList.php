74c6b0b7ab11c71733f374c35d790e25bcf23e81
Krzysztof Klimek <klimek.krzy@gmail.com>
2018-09-07 01:38:08 +0200

    data to TouristActivity

--- touristTourList.php
+++ touristTourList.php
@@ -2,22 +2,37 @@
 
 require_once "connect.php";
 
-$con=mysqli_connect($host, $username, $pwd, $db) or die('unnable to connect');
+$con=mysqli_connect($host, $username, $pwd, $db);
 
 if(mysqli_connect_error($con)){
 	echo "Failed to connect";
 }
 
-$query=mysqli_query($con, "SELECT name FROM tours");
+$string = $_GET['string'];
+
+$query=mysqli_query($con, "SELECT * FROM tours");
+
+$query_user=mysqli_query($con, "SELECT * FROM users WHERE email = '$string'");
+
+$query_user_result = $query_user->fetch_assoc();
+$id = $query_user_result['id'];
 		
-if($query)
-{
-	while($row = $query->fetch_assoc())
-	{
-		//$output = $row['name'];
-		//echo $output;
-		echo $row['name'];
-		echo "/";
+if($query){
+	
+	while($row = $query->fetch_assoc()){
+		
+		$user_id = $row['id'];
+		$query_check_box=mysqli_query($con, "SELECT * FROM userstours WHERE user_id_fk = '$id' AND tour_id_fk = '$user_id'");
+		$how_many_rows = $query_check_box->num_rows;
+		
+		if($how_many_rows>0){
+			echo $row['name'].'/'.$row['date'].'/'.$row['description'].'/true';
+			echo "/";
+		}
+		else{
+			echo $row['name'].'/'.$row['date'].'/'.$row['description'].'/false';
+			echo "/";
+		}
 	}	
 }
 	
