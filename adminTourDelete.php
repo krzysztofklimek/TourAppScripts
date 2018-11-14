9ffa07bb876cf35ce68f920b256267a8a39cc135
Krzysztof Klimek <klimek.krzy@gmail.com>
2018-11-14 20:29:51 +0100

    deleting tours by admin

---
+++ adminTourDelete.php
@@ -0,0 +1,19 @@
+<?php
+
+
+require_once "connect.php";
+$con=mysqli_connect($host, $username, $pwd, $db);
+
+if(mysqli_connect_error($con)){
+	echo "Fail to connect";
+}
+
+$string = $_GET['string'];
+$valueArray = explode('/', $string);
+
+
+$query = mysqli_query($con, "DELETE FROM tours WHERE name = '$valueArray[0]' AND date = '$valueArray[1]'");
+
+
+mysqli_close($con);
+?>
